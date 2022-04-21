5b8df6fd90c8ad2d9c7dc48d4364b7c3af9c69fe
Jonatan-horton <jonatan.horton@gmail.com>
2022-04-21 15:42:52 -0500

    removed comment lines

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
-/* Middleware*/
-// const bodyParser = require('body-parser')
-
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(express.urlencoded({extended: false})); 
 app.use(express.json());
@@ -17,8 +14,6 @@ app.use(express.json());
 const cors = require('cors');
 const { response } = require('express');
 app.use(cors());
-
-// Cors for cross origin allowance
 
 // Initialize the main project folder
 app.use(express.static('website'));
